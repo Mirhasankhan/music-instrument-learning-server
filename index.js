a4ae6d54e11c65be49d1a645f6b2b2790bf9f59e
Mirhasankhan <mirhasan000034@gmail.com>
2023-06-10 13:47:57 +0600

    admin api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -27,15 +27,43 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const usersCollection = client.db('users').collection('user')
-
-        app.get('/users', async(req, res)=>{
+        // All user apis
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
 
-        app.post('/users', async(req, res)=>{
-            const user = req.body;
+        app.post('/users', async (req, res) => {
+            const user = req.body;          
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
             const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedUser = {
+                $set: {
+                    role: 'instructor'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedUser)
+            res.send(result)
+        })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedUser = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedUser)
             res.send(result)
         })
         // Send a ping to confirm a successful connection
